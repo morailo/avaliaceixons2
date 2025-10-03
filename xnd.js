0f317e7e6fff37b81cb3b5ec825bcb6f4fbfff24
xande <alexandre.morais.amaral@escola.pr.gov.br>
2025-10-03 10:01:26 -0300

    Add files via upload

---
+++ xnd.js
@@ -0,0 +1,88 @@
+const tagnome = document.querySelector("#nome");
+const tagemail = document.querySelector("#email");
+const tagsenha = document.querySelector("#senha");
+const btnCadastro = document.querySelector("#cadastro");
+
+let cadastro = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+let usuariosLogados = JSON.parse(localStorage.getItem("usuarioLogados")) || null;
+
+const tagloginemail = document.querySelector("#emaillogin");
+const tagloginsenha = document.querySelector("#senhalogin");
+const btnLogin = document.querySelector("#login");
+
+const  btnSair = document.querySelector("#desconectar");
+const exibirnome = document.querySelector("#exibirUsuario");
+
+if(window.location.pathname.endsWith("xnd.html")&& usuariosLogados){
+  window.location.href = "pagina2";
+}
+
+if(window.location.pathname.endsWith("pagina2.html")&& !usuariosLogados){
+  window.location.href = "xnd.html";
+}
+
+if(window.location.pathname.endsWith("xnd.html")){
+
+btnCadastro.addEventListener("click" , function(){
+     const nome = tagnome.value
+     const email = tagemail.value
+     const senha = tagsenha.value
+     if (nome === "" | email ==="" | senha ==="" ) {
+    return (alert ("Preencha todos os campos!") )
+    } 
+
+    else {
+     if (email.includes ("@" + ".")) {
+
+     const usuario = {nome: nome , email:  email , senha: senha} ;
+     cadastro.push(usuario);
+     localStorage.setItem("usuarios" , JSON.stringify(cadastro));
+     alert ("Cadastrado com sucesso!") 
+     console.log(cadastro);
+      tagnome.value = "" 
+      tagemail.value = ""  
+      tagsenha.value = ""
+
+    }
+    else {
+      
+      return(alert ("O Email '"+ email + "' não é válido, tente novamente"))
+    }
+  };
+ }
+);
+    btnLogin.addEventListener("click" , function(){
+
+    const emaillogin = tagloginemail.value
+    const senhalogin = tagloginsenha.value
+
+    const existe = cadastro.find(function(usuario){
+        return usuario.email === emaillogin && usuario.senha === senhalogin
+
+    })
+    
+    if (existe) {
+      Logados = {nome: existe.nome , email:  existe.email};
+      localStorage.setItem("usuarioLogados", JSON.stringify(Logados));
+      alert("Login realizado com sucesso!")
+      window.location.href = "pagina2.html"
+    }
+     else{
+      alert("O usuario não foi encontrado!")      
+    }
+    tagloginemail.value = ""
+    tagloginsenha.value = ""
+  }
+)
+
+}
+if(window.location.pathname.endsWith("pagina2.html")){
+btnSair.addEventListener("click" , function(){
+  localStorage.removeItem("usuarioLogados");
+  usuariosLogados = null;
+  window.location.href = "xnd.html"
+
+}
+)
+}
